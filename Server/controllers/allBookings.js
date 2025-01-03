b65808f81a3a66be8900ea64e2606d484712b33d
obsideen <97191699+Pritthish20@users.noreply.github.com>
2025-01-03 22:38:41 +0530

    Update allBookings.js

--- Server/controllers/allBookings.js
+++ Server/controllers/allBookings.js
@@ -1,18 +1,12 @@
 import Bookings from "../models/Bookings.js";
 
-const getAllBookings = async (req, res) => {
-  try {
-    const bookings = await Bookings.find();
-    return res.status(200).json({
-      success: true,
-      data: bookings,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-};
+const getAllBookings=async (req, res) =>{
+    try {
+        const bookings = await Bookings.find();
+        return res.json(bookings);
+    } catch (error) {
+        req.status(500).json({error:error.message});
+    }
+}
 
 export default getAllBookings;
